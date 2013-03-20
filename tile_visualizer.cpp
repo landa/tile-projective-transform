e47387349dc91a29a79b8bebff6da5adb07c0f60
Yafim Landa <landa@mit.edu>
2013-03-20 12:30:58 -0400

    incorporated Javier's advice

--- tile_visualizer.cpp
+++ tile_visualizer.cpp
@@ -40,6 +40,30 @@ void drawTile(GLfloat tile_definition[4][3]) {
   glEnd();
 }
 
+void drawTileOutline(GLfloat tile_definition[4][3]) {
+  glLineWidth(10.0f);
+  glBegin(GL_LINE_LOOP);
+  glColor3f(1.0, 1.0, 1.0);
+  glVertex3fv(&tile_definition[3][0]);
+  glVertex3fv(&tile_definition[2][0]);
+  glVertex3fv(&tile_definition[1][0]);
+  glVertex3fv(&tile_definition[0][0]);
+  glEnd();
+  glBegin(GL_LINES);
+  for (int ii = 0; ii < 20; ii++) {                                    
+    glVertex3f(cv::Point(0, (int) ii*spacing),
+    glVertex3f(cv::Point(old.cols, (int) ii*spacing));    
+
+    glVertex3f(0, (int) ii*spacing),
+    glVertex3f(old.cols, (int) ii*spacing));    
+  }                                                             
+  spacing = old.cols/20.0;                               
+  for (int ii = 0; ii < 20; ii++) {                                             
+    line(old, cv::Point((int) ii*spacing, 0), cv::Point((int) ii*spacing, old.rows), cv::Scalar(0, 255, 0), 3);    
+  }
+  glEnd();
+}
+
 GLuint loadTexture() {
   textureId = SOIL_load_OGL_texture(
     "scene_image.png",
@@ -85,32 +109,33 @@ void draw() {
 
   glTranslated(0.5, 0.5, 0.5); // Scale and bias the [-1,1] NDC values
   glScaled(0.5, 0.5, 0.5); // to the [0,1] range of the texture map
-  gluPerspective(40.0f, 1024/768.0f, 0.1f, 100.0f);
+  gluPerspective(40.0f, 3/2.0f, 0.1f, 100.0f);
 
   // move the projector to the camera pose
-  glTranslatef(camera_origin[0], camera_origin[1], camera_origin[2]);
+  glMatrixMode(GL_MODELVIEW);
   glRotatef(camera_angle, 0.0f, 1.0f, 0.0f);
+  glTranslatef(camera_origin[0], camera_origin[1], camera_origin[2]);
 
   // gluLookAt(0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 
 
 
-  // Set up the camera
+  // Set up the virtual camera
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   glPushMatrix();
-  gluPerspective(40.0f, 1024/768.0f, 0.1f, 100.0f);
+  gluPerspective(53.13f, 1.0f, 0.1f, 100.0f);
   fprintf(stderr, "moving camera right by %f, up by %f, back by %f\n",
                   camera_origin[0], camera_origin[1], camera_origin[2]);
-  glTranslatef(camera_origin[0], camera_origin[1], camera_origin[2]);
-  glRotatef(camera_angle, 0.0f, 1.0f, 0.0f);
-  GLfloat tile_19_camera[3];
-  GLfloat tile_19_offset[3];
-  vec_scalar_mult(tile_19_normal, 1.0f, tile_19_offset);
-  vec_add(tile_19_origin, tile_19_offset, tile_19_camera);
+  // glRotatef(camera_angle, 0.0f, 1.0f, 0.0f);
+  // glTranslatef(camera_origin[0], camera_origin[1], camera_origin[2]);
+  GLfloat tile_17_camera[3];
+  GLfloat tile_17_offset[3];
+  vec_scalar_mult(tile_17_normal, 1.1f, tile_17_offset);
+  vec_add(tile_17_origin, tile_17_offset, tile_17_camera);
   // gluLookAt(0.0f, 10.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f);
-  // gluLookAt(tile_19_camera[0], tile_19_camera[1]-2.0f, tile_19_camera[2], tile_19_origin[0], tile_19_origin[1]-2.0f, tile_19_origin[2], 0.0f, 1.0f, 0.0f);
+  gluLookAt(tile_17_camera[0], tile_17_camera[1]-2.0f, tile_17_camera[2], tile_17_origin[0], tile_17_origin[1]-2.0f, tile_17_origin[2], 0.0f, 1.0f, 0.0f);
 
 
 /*
@@ -167,22 +192,38 @@ drawTile(tile_27_corners);
 drawTile(tile_28_corners);
 drawTile(tile_29_corners);
 drawTile(tile_30_corners);
-drawTile(tile_31_corners);
-drawTile(tile_32_corners);
-drawTile(tile_33_corners);
-drawTile(tile_34_corners);
-drawTile(tile_35_corners);
-drawTile(tile_36_corners);
-drawTile(tile_37_corners);
-drawTile(tile_38_corners);
-drawTile(tile_39_corners);
-drawTile(tile_40_corners);
-drawTile(tile_41_corners);
-drawTile(tile_42_corners);
-drawTile(tile_43_corners);
-drawTile(tile_44_corners);
-drawTile(tile_45_corners);
-drawTile(tile_46_corners);
+
+drawTileOutline(tile_0_corners);
+drawTileOutline(tile_1_corners);
+drawTileOutline(tile_2_corners);
+drawTileOutline(tile_3_corners);
+drawTileOutline(tile_4_corners);
+drawTileOutline(tile_5_corners);
+drawTileOutline(tile_6_corners);
+drawTileOutline(tile_7_corners);
+drawTileOutline(tile_8_corners);
+drawTileOutline(tile_9_corners);
+drawTileOutline(tile_10_corners);
+drawTileOutline(tile_11_corners);
+drawTileOutline(tile_12_corners);
+drawTileOutline(tile_13_corners);
+drawTileOutline(tile_14_corners);
+drawTileOutline(tile_15_corners);
+drawTileOutline(tile_16_corners);
+drawTileOutline(tile_17_corners);
+drawTileOutline(tile_18_corners);
+drawTileOutline(tile_19_corners);
+drawTileOutline(tile_20_corners);
+drawTileOutline(tile_21_corners);
+drawTileOutline(tile_22_corners);
+drawTileOutline(tile_23_corners);
+drawTileOutline(tile_24_corners);
+drawTileOutline(tile_25_corners);
+drawTileOutline(tile_26_corners);
+drawTileOutline(tile_27_corners);
+drawTileOutline(tile_28_corners);
+drawTileOutline(tile_29_corners);
+drawTileOutline(tile_30_corners);
   glPopMatrix();
 
   glDisable(GL_TEXTURE_2D);
